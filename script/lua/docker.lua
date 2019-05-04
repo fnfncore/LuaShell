615ac6a4cf644ad2b498d5348ff5e98ee544d557
gc87 <sfme@qq.com>
2019-05-04 10:37:18 +0800

    [#]rpc test.

--- script/lua/docker.lua
+++ script/lua/docker.lua
@@ -229,20 +229,136 @@ function docker.getforkcount_test1()
     print('getforkcount testing passed')
 end
 
-function docker.getforkcount_test2()
+function docker.getforkcount_test2(n, c)
+    local start_time = os.time()
     local i = 0
     local count = 0
     repeat
         local err
         local ret
         err, ret = rpc.callhost('getforkcount', docker.rootrpchost, docker.rootrpcport)
-        if 0 == err and 3 == ret then
+        if 0 == err and c == ret then
             count = count + 1
         end
         i = i + 1
         print('getforkcount err:' .. err .. ', ret:' .. ret .. ', index:' .. i)
-    until(i >= 10000)
-    print('getforkcount testing, normal:' .. count)
+    until(i >= n)
+    local end_time = os.time()
+    local total_time = end_time - start_time;
+    local average_time = total_time / n
+    print('getforkcount testing, normal:' .. count .. ', time:' .. total_time .. ', avg time:' .. average_time)
+end
+
+docker.forks = {
+    'd62c1fca5f2aacf9cf5738ed057d9987373508d8984734fa8fac05a6780a7cfd',
+    'c3c7d25961b7f3b5a69d4b0b3756ec856815a596fb30c60346b15c7a66e087b9',
+    '7303aa24ffa184d63f618d67750cbaac32377676a591879739f2b9899c39f346',
+    'f1405b170aa36ed1fa0462b065536bd0ff231e9cb6aa7c38586a0df2c38ae46e',
+    '6695266140184418c1f568b85dba8d4470bd4b09ab12618c4066ff3b25296a2c'
+}
+
+function is_in_table(value, tbl)
+    for k,v in ipairs(tbl) do
+        if v == value then
+            return true;
+        end
+    end
+    return false;
+end
+
+function docker.listfork(n, c)
+    local i = 0
+    local count = 0
+    local start_time = os.time()
+    repeat
+        local err
+        local ret
+        err, ret = rpc.callhost('listfork', docker.rootrpchost, docker.rootrpcport)
+        if 0 == err and c == #ret then
+            count = count + 1
+        end
+        i = i + 1
+        print('listfork err:' .. err .. ', ret:' .. ret .. ', index:' .. i)
+    until(i >= n)
+    local end_time = os.time()
+    local total_time = end_time - start_time;
+    local average_time = total_time / n
+    print('listfork testing, normal:' .. count .. ', time:' .. total_time .. ', avg time:' .. average_time)
+end
+
+--getblockcount\getblocklocation\getblockhash\getblock
+--docker.cross_test_fork = 'f1405b170aa36ed1fa0462b065536bd0ff231e9cb6aa7c38586a0df2c38ae46e'
+docker.cross_test_fork = 'd62c1fca5f2aacf9cf5738ed057d9987373508d8984734fa8fac05a6780a7cfd'
+function docker.cross_test(test_fork)
+    local count = 0
+    local err
+    local ret
+    err, ret = rpc.callhost('getblockcount', docker.rootrpchost, docker.rootrpcport, docker.cross_test_fork)
+    print('getblockcount:' .. ret)
+    if 0 == err then
+        for i = 0, 280 do
+            local err1, ret1 = rpc.callhost('getblockhash', docker.rootrpchost, docker.rootrpcport, i, docker.cross_test_fork)
+            print(i .. '-1-getblockhash i:' .. i .. ', hash:' .. ret1[1])
+            if err1 == 0 then
+                local err2, ret2 = rpc.callhost('getblock', docker.rootrpchost, docker.rootrpcport, ret1[1])
+                print(i .. '-2-getblock fork:' .. ret2['fork'] .. ', hash:' .. ret2['hash'])
+                if err2 == 0 then
+                    local err3, ret3 = rpc.callhost('getblocklocation', docker.rootrpchost, docker.rootrpcport, ret2['hash'])
+                    print(i .. '-3-getblocklocation fork:' .. ret3['fork'] .. ', height' .. ret3['height'])
+                    if err3 == 0 and ret3['height'] == i and docker.cross_test_fork == ret3['fork'] then
+                        count = count + 1;
+                    end
+                end
+            end
+        end
+    end
+    print("cross test normal count:" .. count)
+end
+
+function docker.getblockcount(fork, n, c)
+    local start_time = os.time()
+    local i = 0
+    local count = 0
+    repeat
+        local err
+        local ret
+        err, ret = rpc.callhost('getblockcount', docker.rootrpchost, docker.rootrpcport, fork)
+        if 0 == err and c == ret then
+            count = count + 1
+        end
+        i = i + 1
+        print('getblockcounbt err:' .. err .. ', ret:' .. ret .. ', index:' .. i)
+    until(i >= n)
+    local end_time = os.time()
+    local total_time = end_time - start_time;
+    local average_time = total_time / n
+    print('getblockcount testing, normal:' .. count .. ', time:' .. total_time .. ', avg time:' .. average_time)
+end
+
+function docker.getblocklocation(test_hash, n)
+    local start_time = os.time()
+    local i = 0
+    local count = 0
+    repeat
+        local err
+        local ret
+        err, ret = rpc.callhost('getblocklocation', docker.rootrpchost, docker.rootrpcport, test_hash)
+        if 0 == err then
+            count = count + 1
+        end
+        i = i + 1
+        print('getblocklocation err:' .. err .. ', index:' .. i)
+    until(i >= n)
+    local end_time = os.time()
+    local total_time = end_time - start_time;
+    local average_time = total_time / n
+    print('getblocklocation testing, normal:' .. count .. ', time:' .. total_time .. ', avg time:' .. average_time)
+end
+
+function docker.getblockhash()
+end
+
+function docker.getblock()
 end
 
 function docker.getblockhash()
@@ -261,19 +377,19 @@ function docker.getblockhash()
     print('getblockhash testing, normal:' .. count)
 end
 
-function docker.listtransaction()
+function docker.listtransaction(n)
     local i = 0
     local count = 0
     repeat
         local err
         local ret
-        err, ret = rpc.callhost('listtransaction', docker.rootrpchost, 6812)
+        err, ret = rpc.callhost('listtransaction', docker.rootrpchost, docker.rootrpcport)
         if 0 == err then
             count = count + 1
         end
         i = i + 1
         print('listtransaction  err:' .. err .. ', count:' .. count)
-    until(i >= 10000)
+    until(i >= n)
     print('listtransaction testing, normal:' .. count)
 end
 
